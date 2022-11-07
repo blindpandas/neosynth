7fcb8774461ae22d7bf3a76e01487a874d8517de
mush42 <musharraf.omer@gmail.com>
2022-11-07 22:17:38 +0200

    Simplified the code and usage."

--- usage.py
+++ usage.py
@@ -9,7 +9,15 @@ import neosynth
 
 FORMAT = '%(levelname)s %(name)s %(asctime)-15s %(filename)s:%(lineno)d %(message)s'
 logging.basicConfig(format=FORMAT)
-
+SSML = """
+<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="en">
+<s>Hello there!</s>
+<mark name="mark1"/>
+<p>Here comes a scilence</p>
+<break time="1500ms"/>
+<s>Goodbye!</s>
+</speak>
+""".strip()
 
 class EventSink:
     """
@@ -33,24 +41,8 @@ def main():
     synth.set_pitch(50.0)
     synth.set_rate(30.0)
     synth.set_volume(75.0)
-    # create the speech utterance
-    ut = neosynth.SpeechUtterance()
-    ut.add_text("Hello there.")
-    ut.add_text("And another thing.")
-    ut.add_text("Goodbye!")
-    ut.add_ssml("""
-        <speak version="1.0" xml:lang="en-US">
-          <s>Hello Musharraf</s>
-          <mark name="mark1"/>
-          <s>Rust is the best</s>
-          <mark name="mark2"/>
-          <s>Bye</s>
-          <mark name="it_works"/>
-        </speak>
-    """.strip())
-    # Speak 
-    synth.speak(ut)
-
+    synth.speak_text("Hello")
+    synth.speak_ssml(SSML)
 
 if __name__ == '__main__':
     main()
